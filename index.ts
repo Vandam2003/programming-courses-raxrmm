a1e5aa6592cd44ac0b20ef4c4a85e4554d588c31
Vandam2003 <125286767+Vandam2003@users.noreply.github.com>
2023-02-18 00:23:26 +0000

    1 file modified

--- index.ts
+++ index.ts
@@ -7,17 +7,17 @@ let num45: number;
 let num50: number;
 num20 = 5000;
 num45 = 5000;
-num50 = 5000;
-console.log (num20 + num45);
+num50 = nm20 + num45 ;
+console.log (num50);
 
 let num25: number;
 let num26: number;
 let num27: number;
 let num28: number;
-num25 = 25
-num26 = 25
-num27 = 25
-num28 = 25
+num25 = 25;
+num26 = 25;
+num27 = 25;
+num28 = 25;
 
 console.log (num25 + num26 + num27 + num28 );
 
