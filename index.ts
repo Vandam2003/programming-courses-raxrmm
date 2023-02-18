d4af2880a578ffa4d9b7e1114082311011bf704e
Vandam2003 <125286767+Vandam2003@users.noreply.github.com>
2023-02-18 13:33:48 +0000

    1 file modified

--- index.ts
+++ index.ts
@@ -2,36 +2,6 @@
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //######################################################################
-let num20: number;
-let num45: number;
-let num50: number;
-num20 = 5000;
-num45 = 5000;
-num50 = num20 + num45 ;
-console.log (num50);
-
-let num25: number;
-let num26: number;
-let num27: number;
-let num28: number;
-num25 = 25;
-num26 = 25;
-num27 = 25;
-num28 = 25;
-
-console.log (num25 + num26 + num27 + num28 );
-
-let num55: number;
-let num30: number;
-num55 = 1500
-num30 = 500
-console.log (num55 - num30);
-
-let num1: number;
-let num2: number;
-num1 = 500
-num2 = 500
-console.log (num1*num2);
 
 let myText ; String
 myText = "going so far";
