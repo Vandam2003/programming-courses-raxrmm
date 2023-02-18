b1064aad244eaee5b4741687161768bc8b4a7131
Vandam2003 <125286767+Vandam2003@users.noreply.github.com>
2023-02-18 14:34:44 +0000

    1 file modified

--- index.ts
+++ index.ts
@@ -7,7 +7,27 @@ let num1: number;
 let num2: number;
 let num3: number;
 
-num1 = 10;
-num2 = 20;
-num3 = num1 + num2 ; 
+num1 = 30;
+num2 = 40;
+num3 = num1 * num2 ; 
 console.log (num3);
+
+let myTxet : String;
+myTxet = "sstech السلام عليكم , انا اتعلم البرمجة على قناة"
+myTxet = myTxet + "وانا متحمس جدا"
+console.log(myTxet);
+
+let num20 : number;
+num20 = 100 + 10 ;
+console.log (num20)
+
+let myNumber : number;
+myNumber =100 ;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+let myNumber1 : number;
+let myNumber2 : number;
+myNumber1 = 10 + 10;
+myNumber2 = 20 - 10;
+console.log (myNumber)
